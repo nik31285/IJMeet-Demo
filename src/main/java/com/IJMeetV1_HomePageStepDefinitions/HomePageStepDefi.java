18f5c002ad33f77aca8ef3568ae68075169e763c
Nishikant <nishan3d@gmail.com>
2022-07-20 16:44:24 +0530

    Schedule meeting scenario completed

--- src/main/java/com/IJMeetV1_HomePageStepDefinitions/HomePageStepDefi.java
+++ src/main/java/com/IJMeetV1_HomePageStepDefinitions/HomePageStepDefi.java
@@ -13,13 +13,18 @@ import com.IJMeet_V1.keywords.UIKeywords;
 import io.cucumber.java.en.*;
 
 public class HomePageStepDefi {
+	@Given("Launch browser and open URL")
+	public void Launch_browser_and_open_URL() throws IOException {
+		UIKeywords.launchBrowser();
+		UIKeywords.launchURL();
+	}
 
 	@Then("to check visibility of IJMeet Logo")
 	public void logo_visibility_check() throws IOException {
 		UIKeywords.isVisible();
 	}
 	
-	@Given("User clicks on sign in button")
+	@And("User clicks on sign in button")
 	public void user_clicks_on_sign_in_button() throws IOException {
 		UIKeywords.click("signInButton_xpath");
 	}
@@ -44,8 +49,67 @@ public class HomePageStepDefi {
 		UIKeywords.click("schedule_meeting_css");
 	}
 	@Then("select TimeZone")
-	public void select_TimeZone() {
-//		UIKeywords.selectDropdown("Time_Zone_css","(GMT+05:30) Asia/Kolkata");
+	public void select_TimeZone() throws IOException {
+		UIKeywords.selectDropdown("Time_Zone_css","(GMT+05:30) Asia/Kolkata");
+	}
+	@And("Enter Meeting Topic")
+	public void Enter_Meeting_Topic() throws IOException {
+		UIKeywords.EnterText("Meeting_Topic_css", "Meeting_Topic_text");
+	}
+	@And("Enter Meeting Description")
+	public void Enter_Meeting_Description() throws IOException {
+		UIKeywords.EnterText("Meeting_Description_css","Meeting_Description_text");
+	}
+	@And("Select date")
+	public void select_date() throws IOException {
+		UIKeywords.click("Meeting_Date_css");
+		UIKeywords.selectDropdown("Month_Select_css","Jul");
+		UIKeywords.selectDropdown("Year_Select_xpath","2022");
+		UIKeywords.click("Day");
+	}
+	@And("Select Time")
+	public void select_time() throws IOException {
+		UIKeywords.click("MeetingTime_css");
+		UIKeywords.clear("MeetingTime_css");
+		UIKeywords.EnterText("MeetingTime_css","MeetingTime");
+	}
+	@And("Set Meeting Duration")
+	public void set_Meeting_Duration() throws IOException {
+		UIKeywords.selectDropdown("Duration_hours_css","04");
+		UIKeywords.selectDropdown("Duration_minutes_css","25");
+	}
+	@And("select Email Template Launguage")
+	public void select_Email_Template_() throws IOException {
+		UIKeywords.selectDropdown("Email_Template_Laungauge_css","English");
+	}
+	@And("check on Require meeting passcode")
+	public void check_Require_meeting_() throws IOException {
+		UIKeywords.click("Require_meeting_passcode_xpath");
+	}
+	@And("Enter Meeting Passcode")
+	public void Enter_Meeting_Passcode() throws IOException {
+		UIKeywords.EnterText("Meeting_passcode_css","Meeting_passcode");
+	}
+	@Then("check on Enable waiting room")
+	public void check_on_enable_waiting_room() throws IOException {
+	    UIKeywords.click("Enable_waiting_room_xpath");
+	}
+
+	@Then("check on Enable restricted mode")
+	public void check_on_enable_restricted_mode() throws IOException {
+	    UIKeywords.click("Enable_restricted_mode_xpath");
+	}
+	@And("on Host Video while starting meeting")
+	public void on_Host_Video_while_starting_meeting() throws IOException {
+		UIKeywords.click("Host_Video_xpath");
+	}
+	@And("check on Mute participants option")
+	public void check_on_Mute_participants_option() throws IOException {
+		UIKeywords.click("Mute_Participant_xpath");
+	}
+	@Then("click on schedule meeting")
+	public void click_on_schedule_meeting() throws IOException {
+		UIKeywords.click("Schedule_Meeting_css");
 	}
 //
 //	@Then("User Navigated to home page")
